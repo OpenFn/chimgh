3c0871fd51e0eac21748ab5128b766203102a984
Taylor Downs <taylor@openfn.org>
2021-04-07 13:13:43 +0100

    Update fetchFromVaccineUPP.js

--- fetchFromVaccineUPP.js
+++ fetchFromVaccineUPP.js
@@ -1,4 +1,11 @@
-get('applicationList', {
-  headers: {"content-type": "application/json"},
-  // authentication: {username: "user", password: "pass"}
-});
+get(
+  'applicationList', // the endpoint
+  {
+    headers: {"content-type": "application/json"}, // some special parameters?
+    // authentication: {username: "user", password: "pass"}
+  },
+  state => { // a callback for viewing or modifying the response before we load to dhis2?
+    console.log('VacinneUPP responded with:', JSON.stringify(state.data, null, 2));
+    return state;
+  }
+);
