86fe7057a5026aa16a6b9db4fdbc6fa1f3f8ddcf
Taylor Downs <taylor@openfn.org>
2021-04-07 13:09:43 +0100

    Create dhis2CreateTei.js

---
+++ dhis2CreateTei.js
@@ -0,0 +1,23 @@
+createTEI({
+   orgUnit: 'TSyzvBiovKh',
+   trackedEntityType: 'nEenWmSyUEp',
+   attributes: [
+      {
+         attribute: 'lZGmxYbs97q',
+         value: valUpsertTEI,
+      },
+      {
+         attribute: 'w75KJ2mc4zz',
+         value: 'Gigiwe',
+      },
+   ],
+   enrollments: [
+      {
+         orgUnit: 'TSyzvBiovKh',
+         program: 'fDd25txQckK',
+         programState: 'lST1OZ5BDJ2',
+         enrollmentDate: '2021-01-04',
+         incidentDate: '2021-01-04',
+      },
+   ],
+});
