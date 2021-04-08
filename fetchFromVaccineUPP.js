be723c7fa2fe048fb8f0ff76eee1449ebaf5c5a2
Taylor Downs <taylor@openfn.org>
2021-04-08 08:11:56 +0100

    taylor@openfn.org's change on OpenFn.org

--- fetchFromVaccineUPP.js
+++ fetchFromVaccineUPP.js
@@ -1,5 +1,6 @@
 get(
-  '/applicationList/region/central',
+  // '/applicationList/region/central',
+  '/applicationList/date/2021-03-07',
   {
     headers: {
       "content-type": "application/json",
@@ -7,7 +8,7 @@ get(
     },
   },
   state => { // a callback for viewing or modifying the response before we load to dhis2?
-    console.log('VacinneUPP responded with:', state.data.length, 'items.');
+    console.log('VacinneUPP responded with:', state.data, 'items.');
     return state;
   }
 );
