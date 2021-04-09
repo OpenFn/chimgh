143662107e409e2935936f820b7650f2db020484
Taylor Downs <taylor@openfn.org>
2021-04-09 13:24:30 +0100

    taylor@openfn.org's change on OpenFn.org

--- dhis2CreateTei.js
+++ dhis2CreateTei.js
@@ -3,26 +3,29 @@ alterState(state => {
    return state;
 });
 
-createTEI({
-   orgUnit: 'TSyzvBiovKh',
-   trackedEntityType: 'nEenWmSyUEp',
-   attributes: [
+each(
+  dataPath('data'),
+  createTEI({
+    orgUnit: 'TSyzvBiovKh',
+    trackedEntityType: 'nEenWmSyUEp',
+    attributes: [
       {
          attribute: 'lZGmxYbs97q',
-         value: 'something',
+         value: dataValue('applicantFirstName'),
       },
       {
          attribute: 'w75KJ2mc4zz',
-         value: 'Gigiwe',
+         value: dataValue('applicantSurname'),
       },
-   ],
-   enrollments: [
+    ],
+    enrollments: [
       {
          orgUnit: 'TSyzvBiovKh',
          program: 'fDd25txQckK',
          programState: 'lST1OZ5BDJ2',
-         enrollmentDate: '2021-01-04',
-         incidentDate: '2021-01-04',
+        // enrollmentDate: '2021-01-04',
+        // incidentDate: '2021-01-04',
       },
-   ],
-});
+    ],
+  })
+);
