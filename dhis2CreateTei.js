855ce693d09b3c4f68557ffffd1c229e9fa5568e
Taylor Downs <taylor@openfn.org>
2021-04-09 13:29:08 +0100

    taylor@openfn.org's change on OpenFn.org

--- dhis2CreateTei.js
+++ dhis2CreateTei.js
@@ -1,31 +1,35 @@
 alterState(state => {
-   console.log('Data from the previous job:', state.data);
+   console.log('Data from the previous job:', state.applicants);
    return state;
 });
 
 each(
-  dataPath('data'),
-  createTEI({
-    orgUnit: 'TSyzvBiovKh',
-    trackedEntityType: 'nEenWmSyUEp',
-    attributes: [
-      {
-         attribute: 'lZGmxYbs97q',
-         value: dataValue('applicantFirstName'),
-      },
-      {
-         attribute: 'w75KJ2mc4zz',
-         value: dataValue('applicantSurname'),
-      },
-    ],
-    enrollments: [
-      {
-         orgUnit: 'TSyzvBiovKh',
-         program: 'fDd25txQckK',
-         programState: 'lST1OZ5BDJ2',
-        // enrollmentDate: '2021-01-04',
-        // incidentDate: '2021-01-04',
-      },
-    ],
-  })
+  dataPath('applicants'),
+  state => {
+    console.log(state.data.reference)
+    return state;
+  }
+  // createTEI({
+  //   orgUnit: 'TSyzvBiovKh',
+  //   trackedEntityType: 'nEenWmSyUEp',
+  //   attributes: [
+  //     {
+  //       attribute: 'lZGmxYbs97q',
+  //       value: dataValue('applicantFirstName'),
+  //     },
+  //     {
+  //       attribute: 'w75KJ2mc4zz',
+  //       value: dataValue('applicantSurname'),
+  //     },
+  //   ],
+  //   enrollments: [
+  //     {
+  //       orgUnit: 'TSyzvBiovKh',
+  //       program: 'fDd25txQckK',
+  //       programState: 'lST1OZ5BDJ2',
+  //       // enrollmentDate: '2021-01-04',
+  //       // incidentDate: '2021-01-04',
+  //     },
+  //   ],
+  // })
 );
