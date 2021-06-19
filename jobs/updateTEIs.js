d75e68de098e3f2024df30c3269a2dc2cb51f92f
Taylor Downs <taylor@openfn.org>
2021-06-19 08:24:15 +0100

    dachaga@icloud.com's change on OpenFn.org

--- jobs/updateTEIs.js
+++ jobs/updateTEIs.js
@@ -55,7 +55,7 @@ each(
           program: dataValue('program_instance'),
           enrollmentDate: dataValue('enrollment_date'),
           incidentDate: dataValue('incident_date'),
-          events: ['events', dataValue('event'),
+          events: ['Event', dataValue('event'),
             {
               program: dataValue('program_instance'),
               orgUnit: dataValue('organisation_unit_UID'),
