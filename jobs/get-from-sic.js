935906e048eb9229436551a46fa9fc9abbc8a664
Taylor Downs <taylor@openfn.org>
2022-05-28 16:09:39 +0100

    taylor@openfn.org's change on OpenFn.org

--- jobs/get-from-sic.js
+++ jobs/get-from-sic.js
@@ -1,14 +1,14 @@
-get(
+ get(
   'https://www.sicappgh.org/api/get-dhis-data',
   // '/applicationList/date/2021-03-07',
   {
     headers: {
       'content-type': 'application/json',
-      // Authorization: state.configuration.password,
+     // Authorization: state.configuration.password,
     },
   },
-  state => {
-    const applicants = state.data.data;
+state => {
+     const applicants = state.data.data;
     console.log();
     return state;
   }
