19a7bbe846a593a3bf0558fad83d3c7aee01c7bc
Taylor Downs <taylor@openfn.org>
2022-09-05 01:35:24 +0100

    dachaga@icloud.com's change on OpenFn.org

--- jobs/get-from-sic.js
+++ jobs/get-from-sic.js
@@ -1,1 +1,7 @@
-get('https://www.sicappgh.org/api/get-dhis-data');
+//get('https://www.sicappgh.org/api/get-dhis-data');
+get('dataValueSets', {
+  dataSet: 'neP6Eyam444',
+  orgUnit: 'Q3prrFtk8WJ',
+  period: 'LAST_MONTH',
+  fields: '*',
+});
