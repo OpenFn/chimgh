b49ba922899d64131917fa9d2f2450fd58397b4d
Taylor Downs <taylor@openfn.org>
2022-10-09 19:17:27 +0100

    dachaga@icloud.com's change on OpenFn.org

--- jobs/get-from-sic.js
+++ jobs/get-from-sic.js
@@ -1,2 +1,19 @@
 //get('https://www.sicappgh.org/api/get-dhis-data');
-get('https://hiv.chimgh.org/staging/api/analytics/dataValueSet.json?dimension=dx:IN_GROUP-eFfqg2b4Tyv&outputIdScheme=ATTRIBUTE:b8KbU93phhz&dimension=pe:202205&dimension=ou:Q3prrFtk8WJ');
+get(
+  "https://dhims.chimgh.org/dhims/api/dataValueSets",
+  {
+    query: {
+      dataSet: "bYPgExf4Gqo", 
+      orgUnit: "smsOYQsusrD",
+      period:  "202206",
+      children: "true"
+    },
+    headers: {"content-type": "application/json"}
+  },
+  state => {
+     console.log("-------------debugging-------------");
+     console.table(state.data.dataValues);
+     console.log("-------------debugging-------------");
+     return state;
+  }
+);
